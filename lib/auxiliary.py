2e2a6e600dd2dc5b9847c670883ad749fd80bc23
Vetinar1 <ralph.mittelweg@arcor.de>
2016-07-07 17:30:38 +0200

    Created landConnections. TODO: airConnections.

--- lib/auxiliary.py
+++ lib/auxiliary.py
@@ -9,8 +9,20 @@ import numpy as np
 #Prozdur zum Einlesen der Daten aus den csv-Dateien.
 #Gibt ein Array mit dem jeweiligen Name der Stadt ('cities'), der Einwohnerzahl ('population'), dem Breitengrad ('latitude')
 #und dem Längengrad ('longitude') aus <- In Subarrays
-def readCities(filename):
-       return np.loadtxt(filename, skiprows=1, delimiter="," ,\
+def readCities(source):
+       return np.loadtxt(source, skiprows=1, delimiter="," ,\
                        dtype={'names': ('cities', 'population', 'latitude', 'longitude'),\
-                              'formats': ('S20', 'i4', 'f8', 'f8')})
+                       'formats': ('S20', 'i4', 'f8', 'f8')})
     
+
+def createNewTemp(source):
+	# Read Cities
+	cities = readCities(source)
+	
+	# Remove cities with more than 200k inhabitants
+	for city in cities:
+		if city[1] < 200000:
+			numpy.delete(cities, city)
+	
+	# Write into new file
+	numpy.savetxt("temp.csv", cities, fmt=["%.20s", "%.1i", "%-.2f", "%-.2f"], delimiter=",", newline="\n")
--- run.py
+++ run.py
@@ -7,6 +7,7 @@
 # Import other scripts (Euler, Runge Kutta, ...)
 #from lib.travel import *
 from lib.auxiliary import *
+import numpy
 
 
 
@@ -15,18 +16,34 @@ from lib.auxiliary import *
 new_temp = input("Create new temp.csv from resources? \n Note: Program does not work without valid temp.csv \n New temp.csv will be used as source \n [y/n]? \n")
 if new_temp == "y":
 	# Create new temp.csv
-	cities = readCities("res/europe.csv")
-	
+	createNewTemp("res/europe.csv")
+
 else:
 	# Do not create new temp.csv
 	# Read temp.csv to create array
-	print("blub")
+	cities = readCities("temp.csv")
 	
+# Initialize connectins between cities
 
+# Initialize Land connections
+# First, create 2D array full of zeros; each row/column index n corresponds to one cities[n]
+landConnections = numpy.zeros((cities.size, cities.size))
 
-# Load data
-# Use https://docs.python.org/3/library/csv.html
-# Name, Format?
+# Put a 1 in each field corresponding to dist < 500 km <= REWORD THIS
+for i in range(cities):
+	for j in range(cities):
+		# If distance between city and city2 < 500km, create connection
+		# NOTE: Distance calculation according to http://www.kompf.de/gps/distcalc.html
+		# -> dist = 6378.388 * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2 - lon1))
+		if i == j:
+			# Obvious rule patch
+			landConnections[i][j] = 0
+		else if 6378.388 * numpy.arccos(numpy.sin(cities[i][2]) * numpy.sin(cities[j][2]) + numpy.cos(cities[i][2]) * numpy.cos(cities[j][2]) * numpy.cos(cities[j][3] - cities[i][3])) < 500:
+			landConnections[i][j] = 1
+
+# Initialize Air connections
+# 
+
 
 while SIMULATION_RUNNING == True:
 	# Run Simulation
