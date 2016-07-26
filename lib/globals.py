4ee9402ed9b6be2a1b0dbe35b6837f925137754d
Vetinar1 <ralph.mittelweg@arcor.de>
2016-07-26 16:07:58 +0200

    Method selection, better drawing

--- lib/globals.py
+++ lib/globals.py
@@ -2,5 +2,5 @@
 
 # TODO: Figure out which of these we actually need
 
-global steps,pt,pf,pl,inputData,cities,population,landConnections,airConnections,probabilitiesAir,probabilitiesLand,sus,inf,rec,beta,gamma,mu,step,m,ax,cont
+global steps,pt,pf,pl,inputData,cities,population,landConnections,airConnections,probabilitiesAir,probabilitiesLand,sus,inf,rec,beta,gamma,mu,step,m,m2,map_ax,cont_ax,cont,method
 
